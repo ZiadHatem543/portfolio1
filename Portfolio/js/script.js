6cda046c7e29a8097e137cd1a28adb9ca5f762b2
ZiadHatem543 <ziad.hatem.hero@gmail.com>
2024-10-07 18:20:05 +0300

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,10 @@
+/**========= typing animation */
+
+var typed = new Typed(".typing", {
+    strings:["Mobile Application Developer","Web Developer","Instructor","Developer"],
+    typeSpeed:100,
+    BackSpeed:50,
+    loop:true
+})
+
+
